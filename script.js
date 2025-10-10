00867070a83e53afbc5fadfe6f076deb8bdeb191
Anugraha05062005 <sridharmoorthyanugraha@gmail.com>
2025-10-10 15:33:36 +0530

    hii\

---
+++ script.js
@@ -0,0 +1,12 @@
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(2, 3));
+function multiply(a, b) {
+    console.log(a * b);
+}
+function divide(a, b) {
+    console.log(a / b);
+}
+multiply(4, 5);
+divide(20, 5);
