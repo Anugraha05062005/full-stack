c7a2dae1dc333b2398167c08b465d1bb4ec4227b
Anugraha05062005 <sridharmoorthyanugraha@gmail.com>
2025-10-13 15:32:02 +0530

    hii

---
+++ anugraha/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1 className="box">Anugraha</h1>
+      <p className="para">welcome to the website. this website is speacally made to lean mearn in my college</p>
+    </div>
+  );
+};
+export default App;
